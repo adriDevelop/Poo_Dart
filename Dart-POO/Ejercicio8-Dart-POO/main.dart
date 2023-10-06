850d88008abbd7ccd383164b6e0f1b683059dab3
Adrian Velasco Carrasco <adridevelop11@gmail.com>
2023-10-06 14:03:24 +0200

    Ejercicio 9

--- Dart-POO/Ejercicio8-Dart-POO/main.dart
+++ Dart-POO/Ejercicio8-Dart-POO/main.dart
@@ -1,8 +1,8 @@
 import 'raices.dart';
 
 void main(){
-  Raices r1 = new Raices(a: 1, b: -4, c: 7);
-  Raices r2 = new Raices(a: 1, b: 3, c: -4);
+  Raices r1 = new Raices(a: 1, b: -5, c: 6);
+  Raices r2 = new Raices(a: 1, b: -4, c: 3);
 
   print('La raiz numero 1 ${r1.caulcular()}');
   print('La raiz numero 2 ${r2.caulcular()}');
